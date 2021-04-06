2c8a88dfd256b6318b7b8013efa8b06fac8dc434
Diego <diego.lavayen@digitalbridge.com>
2021-04-06 11:29:23 +0100

    feat: add propTypes to components

--- src/components/post-list.js
+++ src/components/post-list.js
@@ -1,13 +1,50 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // components
 import PostEntry from './post-entry';
 
 const PostList = ({ posts, users }) => {
-    return (posts || []).map(post => {
+    return posts.map(post => {
         const user = users.find(({ id }) => id === post.userId);
         return <PostEntry key={post.id} post={post} user={user} />;
     });
+};
+
+PostList.propTypes = {
+    posts: PropTypes.arrayOf(
+        PropTypes.shape({
+            userId: PropTypes.number,
+            id: PropTypes.number,
+            title: PropTypes.string,
+            body: PropTypes.string,
+        }),
+    ).isRequired,
+    users: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number,
+            name: PropTypes.string,
+            username: PropTypes.string,
+            email: PropTypes.string,
+            address: PropTypes.shape({
+                street: PropTypes.string,
+                suite: PropTypes.string,
+                city: PropTypes.string,
+                zipcode: PropTypes.string,
+                geo: PropTypes.shape({
+                    lat: PropTypes.string,
+                    lng: PropTypes.string,
+                }),
+            }),
+            phone: PropTypes.string,
+            website: PropTypes.string,
+            company: PropTypes.shape({
+                name: PropTypes.string,
+                catchPhrase: PropTypes.string,
+                bs: PropTypes.string,
+            }),
+        }),
+    ).isRequired,
 };
 
 export default PostList;
