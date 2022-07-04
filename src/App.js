b882e5112ab8b492d2348f4c78a461af4f05f6a5
ben <ben.s@builtbypixel.com>
2022-07-04 17:09:43 +0100

    create starting point for langauges app

--- src/App.js
+++ src/App.js
@@ -1,44 +1,9 @@
 import './App.css';
-import List from "./components/List";
-import {useState} from "react"
 
 function App() {
   const rawData = ["javascript", "python", "php", "C++"]
-
-  const [languages, setLanguages] = useState(rawData)
-  const [total, setTotal] = useState(0)
-  const [newLanguage, setNewLanguage] = useState("")
-  const [userInput, setUserInput] = useState("")
-
-  const handleChange = (event) => {
-    setUserInput(event.target.value)
-  }
-  const handleClick = () => {
-    setLanguages((prev) => {
-      return [...prev, userInput]
-    })
-  }
   
-  return (
-    <div className="App">
-      <section className="center">
-        <h1>Programming languages</h1>
-        <p>Total votes: {total}</p>
-        <h2>Add new language</h2>
-        <label htmlFor="language">language</label>
-        <input 
-          id="language"
-          type="text"
-          onChange={handleChange}
-          value={userInput}
-        />
-        <button onClick={handleClick}>submit</button>
-      </section>
-      <List languages={languages} setTotal={setTotal}/>
-    </div>
-  );
+  return <p>Hi</p>
 }
 
 export default App;
-
-// {languages: languages}
